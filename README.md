c5a6d99d34231b3bbeb1d64c3af416ee6370ef67
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-27 14:28:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-thbpty
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-thbpty)
---
+++ index.js
@@ -0,0 +1,12 @@
+// foreach
+const array1 = ['a','b','c'];
+array,foreach(element => console.log(element));
+
+// sort
+const months = ['march','jan','feb','may'];
+months.sort();
+console.log(months);
+
+// slice
+cost animals = ['hen','tiger','bison','elephant','lion'];
+console.log(animals.slice(4));
